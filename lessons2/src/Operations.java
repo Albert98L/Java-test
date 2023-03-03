d9f9fc5451d85a2088ffe044a16f89e260ec69d7
Albert Levonyan <albertlevonyan13@gmail.com>
2023-03-03 19:34:22 +0400

    made changes on code

---
+++ lessons2/src/Operations.java
@@ -0,0 +1,27 @@
+public class Operations {
+    public static void main(String[] args) {
+        int _x = 9;
+        System.out.println(_x);
+        int a465 = 98;
+        System.out.println(a465);
+
+        String firstName = "Albert";
+        System.out.println(firstName);
+        String first_name = "Albert";
+        System.out.println(first_name);
+        String firstNameOfTheBestStudent = "Albert";
+        System.out.println(firstNameOfTheBestStudent);
+
+        int a = 5;
+        int b = 6;
+        int c = a + b ;
+        System.out.println(c);
+        c = a - b ;
+        System.out.println(a*b);
+
+        int h = 8, j = 5;
+        System.out.println(h/j);
+        System.out.println(h%j);
+
+    }
+}
