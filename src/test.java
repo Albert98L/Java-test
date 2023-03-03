aa59ef65ce1a97155d74ad926c5687330deb6cfe
Albert Levonyan <albertlevonyan13@gmail.com>
2023-03-03 14:13:06 +0400

    made changes on code

--- src/test.java
+++ src/test.java
@@ -1,8 +1,20 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("Lessons1");
 
         byte y = 8;
         System.out.println(y);
+
+        short x = 154;
+        System.out.println(x);
+
+        int z = 12453;
+        System.out.println(z);
+
+        long r=123456L;
+        System.out.println(r);
+
+
     }
 }
